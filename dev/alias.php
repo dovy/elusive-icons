9c5416acf0962393768f53fa161e015772f08126
Dovy <dovy@reduxframework.com>
2015-02-18 17:54:04 -0700

    Centered icons

---
+++ dev/alias.php
@@ -0,0 +1,26 @@
+<?php
+
+    $alias = array(
+        'phone' => array(
+            'earphone'
+        ),
+        'paper-clip' => array(
+            'paperclip'
+        ),
+        'paper-clip-alt' => array(
+            'paperclip-alt'
+        ),
+        'paper-clip-alt' => array(
+            'paperclip-alt'
+        ),
+        'check-empty' => array(
+            'unchecked'
+        ),
+
+    );
+    $filters = array(
+
+    );
+    $categories = array(
+
+    );
--- dev/build.php
+++ dev/build.php
@@ -2,6 +2,8 @@
 
     $icons = json_decode( file_get_contents( 'elusiveicons.css' ), true );
 
+    include('alias_filter_categories.php');
+
     require_once "spyc/spyc.php";
     $data    = Spyc::YAMLLoad( dirname( __FILE__ ) . '/../src/icons.yml' );
     $newyaml = array();
@@ -12,19 +14,43 @@
             $data['icons'][ $key ]['unicode'] = $icons[ $value['id'] ];
             unset( $icons[ $value['id'] ] );
         }
+
     }
+
+
     if ( ! empty( $icons ) ) {
         foreach ( $icons as $icon => $unicode ) {
-            $data['icons'][] = array(
-                'name'       => $icon,
+            $name            = ucwords( str_replace( '-', ' ', $icon ) );
+            $filter_parts = explode('-', $icon);
+            if (isset($filters[$icon])) {
+                foreach($filter_parts as $key) {
+                    if (!in_array($key, $filters[$icon])) {
+                        $filters[$icon][] = $key;
+                    }
+                }
+            } else {
+                $filter = $filter_parts;
+            }
+            if (!in_array($name, $filters[$icon])) {
+                $filters[$icon][] = $name;
+            }
+            if (!in_array($icon, $filters[$icon])) {
+                $filters[$icon][] = $icon;
+            }
+
+            $filters[] = $name;
+            $newIcon = array(
+                'name'       => $name,
                 'id'         => $icon,
                 'unicode'    => $unicode,
                 'created'    => "2.0",
-                'filter'     => array(
-                    $icon
-                ),
+                'filter'     => $filters[$icon],
                 'categories' => array( 'Web Application Icons' )
             );
+            if (isset($alias[$icon])) {
+                $newIcon['aliases'] = $alias[$icon];
+            }
+            $data['icons'][] = $newIcon;
         }
     }
 
